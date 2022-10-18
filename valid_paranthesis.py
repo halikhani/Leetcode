ca1c60a2d22404261c91ce6487b7b8684d8b2924
Hamidreza Alikhani <hamidra@Hamidrezas-MacBook-Pro.local>
2022-10-17 21:16:56 -0700

    valid paranthesis and daily temp solved

--- valid_paranthesis.py
+++ valid_paranthesis.py
@@ -1,76 +1,41 @@
-# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-# An input string is valid if:
-
-# Open brackets must be closed by the same type of brackets.
-# Open brackets must be closed in the correct order.
- 
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
 # Example 1:
-# Input: s = "()"
-# Output: true
-
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
 # Example 2:
-# Input: s = "()[]{}"
-# Output: true
-
-# Example 3:
-# Input: s = "(]"
-# Output: false
- 
+# Input: n = 1
+# Output: ["()"]
 
 # Constraints:
+# 1 <= n <= 8
 
-# 1 <= s.length <= 104
-# s consists of parentheses only '()[]{}'.
-
-
-
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        result = None
-        chars_list = []
-        for c in s:
-            if c == '(' or c == '{' or c == '[':
-                chars_list.append(c)
-            else:
-                if c == ')':
-                    if len(chars_list) == 0:
-                        result = False
-                        return
-                    else:
-                        if chars_list[-1] == '(':
-                            del chars_list[-1]
-                        else:
-                            result = False
-                            return
-                if c == '}':
-                    if len(chars_list) == 0:
-                        result = False
-                        return
-                    else:
-                        if chars_list[-1] == '{':
-                            del chars_list[-1]
-                        else:
-                            result = False
-                            return
-                        
-                if c == ']':
-                    if len(chars_list) == 0:
-                        result = False
-                        return
-                    else:
-                        if chars_list[-1] == '[':
-                            del chars_list[-1]
-                        else:
-                            result = False
-                            return
-                    
-        result = True if len(chars_list) == 0 else False
-        return result
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # code with getting help from neetcode
+        
+        # only add open paranthesis when open < n
+        # only add closed paranthesis when closed < open
+        # valid IIF open == closed == n
+        
+        stack = []
+        res = []
+        
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append(''.join(stack))
+                return
+            
+            if openN < n:
+                stack.append('(')
+                print(stack)
+                backtrack(openN + 1, closedN)
+                stack.pop()
                 
             
+            if closedN < openN:
+                stack.append(')')
+                print(stack)
+                backtrack(openN, closedN + 1)
+                stack.pop()
+                
